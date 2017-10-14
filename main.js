b738ecd87fc3c0486dcb8c79b8cffe653f236e70
Danielle Palmer <daniellezpalmer@gmail.com>
2017-10-14 15:54:02 -0500

    add equal query selector and button click event

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 let button = document.querySelectorAll('.show');
 let displayScreen = document.querySelector('#input');
 let clearBtn = document.querySelector('#btnClr');
+let equalBtn = document.querySelector('#equalSign');
+
+button.forEach(function(clicked) {
+  clicked.addEventListener('click', function() {
+    if (displayScreen.textContent.length < maxLength) {
+      displayScreen.textContent += clicked.value;
+    }
+  })
+})
